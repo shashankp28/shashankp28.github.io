ab4cb0731231f8a3f6434d9e90d73000b34b3943
Shashank P <shashankp2832@gmail.com>
2023-08-25 18:29:50 +0530

    Added template for projects

--- my-portfolio/src/components/Projects/Projects.jsx
+++ my-portfolio/src/components/Projects/Projects.jsx
@@ -1,10 +1,29 @@
 import React from 'react';
 import './Projects.css';
+import data from '../../constants.json';
 
 export const Projects = () => {
+  const projects = data['projects'];
+
   return (
-		<div className="projects" id="projects">
-			<h1>Projects</h1>
-		</div>
-	);
+    <div className="projects" id="projects">
+      <h1 className='project-heading'>Projects</h1>
+      <div className="project-list">
+        {projects.map((project, index) => (
+          <a key={index} href={project.link} target="_blank" rel="noopener noreferrer" className="project-block">
+            <div className="project-content">
+              <img src={project.photo} alt={project.title} className="project-image" />
+              <h2>{project.title}</h2>
+              <p>{project.summary}</p>
+              <ul>
+                {project.description.map((desc, idx) => (
+                  <li key={idx}>{desc}</li>
+                ))}
+              </ul>
+            </div>
+          </a>
+        ))}
+      </div>
+    </div>
+  );
 }
